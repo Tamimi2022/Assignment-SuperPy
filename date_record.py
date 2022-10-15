772f9dea58578b405c369a9e41fe0dac6da7aa82
Farid Attamimi <tamimi2021farid@gmail.com>
2022-10-15 18:07:19 +0200

    Back-End2022

--- date_record.py
+++ date_record.py
@@ -1,5 +1,11 @@
 from datetime import date, datetime, timedelta
+from rich.console import Console
+from rich.theme import Theme
+from rich.table import Table
 
+custom_theme = Theme({'OK': 'green', 'error': 'red'})
+console = Console(theme=custom_theme)
+Table(show_lines=True, header_style='green')
 
 def is_date_record(date_string):
     try:
@@ -15,7 +21,7 @@ def reset_date():
         date_now = today.strftime('%Y-%m-%d')
         file.write(date_now)
         file.close()
-        print(f"Date set to: {date_now}")
+        console.print(f"Date set to: {date_now}")
 
 def advance_time(days):
     with open('date.txt', 'r') as f:
@@ -28,12 +34,12 @@ def advance_time(days):
     with open('date.txt', 'w') as file:
         file.write(new_date_string)
         file.close
-        print(f"Date OK: {new_time}")
+        console.print(f"Date OK: {new_time}")
 
 def change_date(date):
     with open('date.txt', 'w') as file:
         file.write(date)
         file.close
         date_set = datetime.strptime(date, '%Y-%m-%d').date()
-        print('Date set to {}'.format(date_set))
+        console.print('Date set to {}'.format(date_set))
         
